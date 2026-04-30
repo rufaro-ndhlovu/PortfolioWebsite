734430e97a20352ceabcc65753de8e4642279ae6
Rufaro <80164120+rufaro-ndhlovu@users.noreply.github.com>
2026-04-30 17:51:13 +0100

    Refactor Hero, NavBar, and HeroLinks components for improved layout and consistency

--- app/sections/NavBar.tsx
+++ app/sections/NavBar.tsx
@@ -10,7 +10,7 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="w-full mt-0 flex flex-row justify-between items-center sm:px-5 py-3 lg:px-20 bg-transparent">
+    <nav className="w-full mt-0 flex flex-row justify-between items-center px-5 py-3 lg:px-20">
       <Logo />
       <div className="md:flex gap-6 hidden">
         <Link href="/" className="text-[var(--text-hero)]">
