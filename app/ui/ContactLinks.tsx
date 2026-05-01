a88cf9304d22c95c8c364af909afb3d5a53785a9
Rufaro <80164120+rufaro-ndhlovu@users.noreply.github.com>
2026-05-01 10:22:19 +0100

    Add Footer component and integrate it into the Home page layout.

--- app/ui/ContactLinks.tsx
+++ app/ui/ContactLinks.tsx
@@ -5,7 +5,7 @@ import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function ContactLinks() {
   return (
-    <div className="flex  flex-row gap-4 text-3xl sm:right-0 z-100  text-[var(--bg-hero-light)]">
+    <div className="flex flex-row gap-4 text-3xl sm:right-0 z-100  text-[var(--bg-hero-light)]">
       <a
         href="https://github.com/rufaro-ndhlovu"
         target="_blank"
---
+++ app/ui/Footer.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[var(--bg-section)] p-4 flex flex-col md:flex-row justify-center md:justify-between items-center">
+      <div className="text-[var(--pink-light)]">
+        <p>&copy;2026 Rufaro Ndhlovu. All rights reserved.</p>
+      </div>
+      <div className="flex flex-row text-[var(--pink-light)] gap-1 items-center">
+        <p className="">
+          Built with{" "}
+          <span className="text-[var(--pink-main)]">
+            Next.js, React & Tailwind CSS
+          </span>{" "}
+        </p>
+        <FaHeart />
+      </div>
+    </footer>
+  );
+}
