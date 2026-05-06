e4966656deebffb2e18f20db8607b6a3b42e333a
Rufaro <80164120+rufaro-ndhlovu@users.noreply.github.com>
2026-05-06 12:41:37 +0100

    feat: update Projects and ProjectsCard components for improved layout and structure

--- app/ui/ProjectsCard.tsx
+++ app/ui/ProjectsCard.tsx
@@ -20,47 +20,43 @@ export default function ProjectsCard({
   live,
 }: props) {
   return (
-    <div className="p-4 bg-[var(--bg-card)] shadow-md border border-[var(--pink-glow)] rounded-4xl gap-4 flex flex-col h-full justify-between">
+    <div className="p-4 bg-[var(--bg-card)] shadow-md border border-[var(--pink-glow)] rounded-4xl gap-4 flex flex-col lg:flex-row h-full justify-evenly grid grid-cols-1 lg:grid-cols-2">
       <div className="flex justify-center">
-        <Image
-          src={image}
-          alt={`${title} screenshot`}
-          width={500}
-          height={300}
-          className="rounded-lg"
-        />
+        <img src={image} alt={`${title} screenshot`} className="rounded-lg" />
       </div>
 
-      <h3 className=" text-[var(--text-dark)] font-bold sm:text-md lg:text-lg text-[var(--font-playfair)]">
-        {title}
-      </h3>
-      <p className="text-[var(--pink-light)]">{description}</p>
-      <div className=" flex flex-row flex-wrap gap-2">
-        {techStack.map((tech) => (
-          <div
-            key={tech}
-            className="text-xs text-[var(--text-dark)] bg-[var(--bg-section)] rounded-full border border-[var(--pink-glow)] p-2"
-          >
-            {tech}
-          </div>
-        ))}
-      </div>
+      <div className="flex flex-col justify-evenly gap-2 lg:gap-0">
+        <h3 className=" text-[var(--text-dark)] font-bold sm:text-md lg:text-lg text-[var(--font-playfair)]">
+          {title}
+        </h3>
+        <p className="text-[var(--pink-light)]">{description}</p>
+        <div className=" flex flex-row flex-wrap gap-2">
+          {techStack.map((tech) => (
+            <div
+              key={tech}
+              className="text-xs text-[var(--text-dark)] bg-[var(--bg-section)] rounded-full border border-[var(--pink-glow)] p-2"
+            >
+              {tech}
+            </div>
+          ))}
+        </div>
 
-      <div className="flex flex-row gap-4">
-        {github && (
-          <button className="text-[var(--pink-light)] font-bold border border-[var(--pink-glow)] rounded-full p-2 hover:bg-[var(--circle-pink)] hover:text-[var(--bg-dark)] text-sm md:text-base">
-            <a href={github} target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </button>
-        )}
-        {live && (
-          <Button onClick={() => {}} color="pink-main">
-            <a href={live} target="_blank" rel="noopener noreferrer">
-              Live
-            </a>
-          </Button>
-        )}
+        <div className="flex flex-row gap-4">
+          {github && (
+            <button className="text-[var(--pink-light)] font-bold border border-[var(--pink-glow)] rounded-full p-2 hover:bg-[var(--circle-pink)] hover:text-[var(--bg-dark)] text-sm md:text-base">
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </button>
+          )}
+          {live && (
+            <Button onClick={() => {}} color="pink-main">
+              <a href={live} target="_blank" rel="noopener noreferrer">
+                Live
+              </a>
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
